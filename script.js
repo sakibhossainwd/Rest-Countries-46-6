3ecf504f31452c556296bf0ae4eb845d96c6be5d
sakibhossain <sakibhossainceo@gmail.com>
2023-09-16 23:28:07 -0700

    container added

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ const loadCountries = () => {
 const displayCountries = countries => {
     console.log(countries);
     const countriesHTML = countries.map(country => getCountryHTML(country))
+    console.log(country);
+    const constainer = document.getElementById('git')
 }
 
 const getCountryHTML = country => {
-    
+    return `
+    <div>
+    <h3>${country}</h3>
+    </div>
+    `
 
 }
 
