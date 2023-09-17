6ed05db152054cdfcab27d4032f81c42ddb0e9c0
sakibhossain <sakibhossainceo@gmail.com>
2023-09-16 23:24:00 -0700

    fetch link added

---
+++ script.js
@@ -0,0 +1,17 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country))
+}
+
+const getCountryHTML = country => {
+    
+
+}
+
+loadCountries();
